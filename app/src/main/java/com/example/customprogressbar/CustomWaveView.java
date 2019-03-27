d557107326d0540c97de504f7f073d00030220fa
preeti.sethi <preeti.sethi@careers360.com>
2019-03-27 18:34:18 +0530

    code

--- app/src/main/java/com/example/customprogressbar/CustomWaveView.java
+++ app/src/main/java/com/example/customprogressbar/CustomWaveView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
@@ -11,8 +12,7 @@ import android.view.View;
 public class CustomWaveView extends View {
 
   private Context mContext;
-  private Paint paint;
-  private Paint shadow_paint;
+  private Paint stroke_paint, shadow_paint, fill_paint;
 
   public CustomWaveView(Context context) {
     super(context);
@@ -32,15 +32,17 @@ public class CustomWaveView extends View {
   private void init(Context context) {
     mContext = context;
 
-    paint = new Paint();
-    paint.setDither(true);
-    paint.setAntiAlias(true);
-    paint.setColor(Color.parseColor("#E1E3EF"));
-    paint.setStyle(Paint.Style.STROKE);
-    paint.setStrokeCap(Paint.Cap.ROUND);
-    paint.setStrokeJoin(Paint.Join.ROUND);
-    paint.setStrokeWidth(dpToPx(5));
+    //set stroke paint
+    stroke_paint = new Paint();
+    stroke_paint.setDither(true);
+    stroke_paint.setAntiAlias(true);
+    stroke_paint.setColor(Color.parseColor("#E1E3EF"));
+    stroke_paint.setStyle(Paint.Style.STROKE);
+    stroke_paint.setStrokeCap(Paint.Cap.ROUND);
+    stroke_paint.setStrokeJoin(Paint.Join.ROUND);
+    stroke_paint.setStrokeWidth(dpToPx(5));
 
+    //set shadow paint
     shadow_paint = new Paint();
     shadow_paint.setDither(true);
     shadow_paint.setAntiAlias(true);
@@ -49,6 +51,13 @@ public class CustomWaveView extends View {
     shadow_paint.setStrokeCap(Paint.Cap.ROUND);
     shadow_paint.setStrokeJoin(Paint.Join.ROUND);
     shadow_paint.setStrokeWidth(dpToPx(3));
+
+    //set fill paint
+//    fill_paint = new Paint();
+//    fill_paint.setDither(true);
+//    fill_paint.setAntiAlias(true);
+//    fill_paint.setColor(Color.parseColor("#E1E3EF"));
+//    fill_paint.setStyle(Paint.Style.FILL);
   }
 
   @Override
@@ -80,7 +89,7 @@ public class CustomWaveView extends View {
       float y2 = midHeight + amplitude * (float) Math.sin(Math.toRadians(x2) * time);
 
       //border stroke
-      canvas.drawLine(x1, y1, x2, y2, paint);
+      canvas.drawLine(x1, y1, x2, y2, stroke_paint);
 
     }
 
@@ -95,6 +104,17 @@ public class CustomWaveView extends View {
       canvas.drawLine(x1, y1+shadow_offset, x2, y2+shadow_offset, shadow_paint);
     }
 
+    /*fill color loop*/
+//    Path path = new Path();
+//    for (int i = 0; i < 180 * 6; i++) {
+//      float x1 = i;
+//      float y1 = midHeight + amplitude * (float) Math.sin(Math.toRadians(x1) * time);
+//      path.lineTo(x1,y1);
+//      path.lineTo(i,0);
+//
+//      //fill path
+//      canvas.drawPath(path,fill_paint);
+//    }
   }
 
   private float dpToPx(int x){
